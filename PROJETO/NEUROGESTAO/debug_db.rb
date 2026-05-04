b73cd82c351477b0cb34af91a236ab97f44a2616
MarcusBMDev <146892409+MarcusBMDev@users.noreply.github.com>
2026-05-04 14:26:03 -0300

    DELETE
    Remoção de arquivos desnecessários.

--- PROJETO/NEUROGESTAO/debug_db.rb
+++
@@ -1,7 +0,0 @@
-File.open("debug_db_out.txt", "w") do |f|
-  Profissional.where("nome LIKE '%ANA CAROLINA%' OR nome LIKE '%DAYANE%'").each do |prof|
-    f.puts "---#{prof.nome}---"
-    agendamentos = prof.agendamentos.where("dia_semana LIKE '%TERÇA%'")
-    f.puts "TERÇA: #{agendamentos.pluck(:horario, :dia_semana).inspect}"
-  end
-end
--- PROJETO/NEUROGESTAO/temp_import.rb
+++
@@ -1,4 +0,0 @@
-caminho = "storage/planilhas/agendamento.xlsx"
-puts "Iniciando importação do arquivo: #{caminho}"
-ImportadorAgendaService.new(caminho).executar
-puts "Processo finalizado!"
